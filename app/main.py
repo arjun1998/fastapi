90726152a85b6e9f52aa91a24e067fa3411bcbd7
arjun1998 <arjunkumarwk1998@gmail.com>
2025-01-20 03:08:01 +0000

    hashed password

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ from .schemas import Post,createUser
 from . import schemas
 from .database import engine,SessionLocal, get_db
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 
 
@@ -22,7 +23,7 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-
+pwd_context = CryptContext(schemes=["bcrypt"])
 
 @app.get("/posts",response_model=List[schemas.ResponseBody])
 def firstapi(db: Session = Depends(get_db)):
@@ -76,6 +77,9 @@ def updatePostById(id:int,post:Post,db: Session = Depends(get_db)):
 
 @app.post("/users",status_code=status.HTTP_201_CREATED,response_model=schemas.createUserResponseBody)
 def createUsers(user:createUser,db: Session = Depends(get_db)):
+    hashed_pwd=pwd_context.hash(user.password)
+    user.password=hashed_pwd
+
     new_Post = models.Users(**user.model_dump())
     try:
         db.add(new_Post)
